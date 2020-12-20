1211a06058a763e441eda4dbcca562a28d122535
IChieh-Pan <ellenpan7174@gmail.com>
2020-12-20 10:30:32 +0100

    fix fetch api

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function List() {
   const [data, setData] = useState({});
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -13,22 +14,24 @@ function List() {
     const data = await response.json();
     // const item = data.results;
     console.log("data :>> ", data);
-    setData(data);
+
+    const item = Object.values(data);
+    console.log("item :>> ", item);
+    const characters = item[1];
+    console.log("characters :>> ", characters);
+    setCharacters(characters);
   };
-  const item = Object.values(data);
-  console.log("item :>> ", item);
-  const characters = item[1];
-  console.log("characters :>> ", characters);
+
 
   return (
     <div>
       <ul className="grid">
-        {characters.map((characters) => {
-          const { id, name } = characters;
+       {characters && characters.map((characters) => {
+          const { id, name , image} = characters;
           return (
             <li key={id} className="card">
               <a href="#">
-                {/* <img src={image} alt={`${name} Thumbnail`} /> */}
+                <img src={image} alt={`${name} Picture`} /> 
                 <h3>{name}</h3>
               </a>
             </li>
