15909274459a60b375bd6eaf8701ce4d3bf8583c
IChieh-Pan <ellenpan7174@gmail.com>
2020-12-29 09:24:48 +0100

    add scss file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import "./App.scss";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Greet from "./components/Greet";
 import HookCounter from "./components/HookCounter";
 import List from "./components/List";
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import "./App.scss";
 
 
 
